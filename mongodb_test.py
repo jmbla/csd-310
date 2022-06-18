879083300418f749bf6870c7f1eda93c7d55d541
jblack-001 <107741583+jblack-001@users.noreply.github.com>
2022-06-18 02:19:18 -0500

    Create mongodb_test.py

---
+++ mongodb_test.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+url = 'mongodb+srv://admin:admin@cluster0.dlvbmu8.mongodb.net/?retryWrites=true&w=majority';
+
+client = MongoClient(url)
+
+db = client.pytech
+
+print(db.list_collection_names())
